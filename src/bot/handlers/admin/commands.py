09c07fe68fc6b2d76c3bf370982c98274efb1160
SashaLimkov <ghalimov.mansur2@mail.ru>
2022-07-06 08:05:59 +0300

    sad

--- src/bot/handlers/admin/commands.py
+++ src/bot/handlers/admin/commands.py
@@ -71,9 +71,6 @@ async def delete_commands(message: types.Message, state: FSMContext):
 
 
 async def set_consult_group(message: types.Message, state: FSMContext):
-    print(1111)
-    print(ADMINS)
-    print(message.from_user.id)
     if message.from_user.id in ADMINS:
         chanel_id = message.reply_to_message.sender_chat.id
         chat_id = message.chat.id
